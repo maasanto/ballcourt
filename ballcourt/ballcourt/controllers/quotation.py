59f13416f53b6ee68ca57f86bd95cae67ee5edfd
Antoine Maas <antoine.maas@gmail.com>
2025-10-24 15:36:33 +0200

    chore: renamed distance to notice

--- ballcourt/ballcourt/controllers/quotation.py
+++ ballcourt/ballcourt/controllers/quotation.py
@@ -8,26 +8,26 @@ def enforce_booking_notice(quotation, method):
 	Enforces that customers can only book within their allowed advance booking window.
 	Raises an exception if the booking is too far in advance.
 	"""
-	permitted_distance_days = get_permitted_distance(quotation.customer)
+	permitted_notice_days = get_permitted_notice(quotation.customer)
 
-	if not permitted_distance_days:
+	if not permitted_notice_days:
 		return
 
 	if not quotation.items:
 		return
 
-	booking_distance_days = calculate_booking_distance(frappe.get_doc("Resource Booking", quotation.items[-1].item_booking))
+	booking_notice_days = calculate_booking_notice(frappe.get_doc("Resource Booking", quotation.items[-1].item_booking))
 
-	if booking_distance_days > permitted_distance_days:
+	if booking_notice_days > permitted_notice_days:
 		frappe.throw(
-			_(f"This booking is {booking_distance_days} days in advance, but your tier only allows bookings up to {permitted_distance_days} days in advance."),
+			_(f"This booking is {booking_notice_days} days in advance, but your tier only allows bookings up to {permitted_notice_days} days in advance."),
 			title=_("Booking Too Far in Advance")
 		)
 
-def calculate_booking_distance(resource_booking):
+def calculate_booking_notice(resource_booking):
 	"""
 	Calculates the number of days between now and the earliest booking item date.
-	Returns the distance in days (integer).
+	Returns the notice in days (integer).
 	"""
 	if not resource_booking:
 		return 0
@@ -38,9 +38,9 @@ def calculate_booking_distance(resource_booking):
 	# Return the number of days (minimum 0 if booking is in the past/same day)
 	return max(0, days_difference)
 
-def get_permitted_distance(customer):
+def get_permitted_notice(customer):
 	"""
-	Retrieves the permitted booking distance (in days) for a customer.
+	Retrieves the permitted booking notice (in days) for a customer.
 	Returns None if not set, or the number of days as an integer.
 	"""
 	if not customer:
@@ -49,11 +49,11 @@ def get_permitted_distance(customer):
 	customer_group = frappe.db.get_value("Customer", customer, "customer_group")
 	permitted_notice = frappe.db.get_value("Customer Group", customer_group, "custom_permitted_booking_notice")
 
-	if permitted_distance is None:
+	if permitted_notice is None:
 		return None
 
 	# Ensure we return an integer
 	try:
-		return int(permitted_distance)
+		return int(permitted_notice)
 	except (ValueError, TypeError):
 		return None
